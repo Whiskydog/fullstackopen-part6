0a1264d69dbf96d122dfc5492bdd928018dc6019
Whiskydog <alan.varde@gmail.com>
2024-01-05 18:35:56 -0300

    Solve exercise 6.21

--- query-anecdotes/src/requests.js
+++ query-anecdotes/src/requests.js
@@ -4,3 +4,8 @@ export const getAnecdotes = async () => {
   const response = await axios.get('/api/anecdotes');
   return response.data;
 };
+
+export const createAnecdote = async (anecdote) => {
+  const response = await axios.post('/api/anecdotes', anecdote);
+  return response.data;
+};
